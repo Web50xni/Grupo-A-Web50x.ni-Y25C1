ee19bf0f51e6cfb140b64ddafc8a1a392bbc8d95
Abner Amarus <123610455+Abner1710@users.noreply.github.com>
2025-07-17 21:23:20 -0600

    Delete Clase 08 directory

--- Clase 08/Ayuda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomeView, ContactFormView, SupportFormView, GraciasView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='inicio'),
-    path('contacto/', ContactFormView.as_view(), name='contacto'),
-    path('soporte/', SupportFormView.as_view(), name='soporte'),
-    path('gracias/', GraciasView.as_view(), name='gracias'),
-]
